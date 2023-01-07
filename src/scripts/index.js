e953742c2ca7620b6c31471e95dbd7c3f8f11654
Caio <caiob1305@gmail.com>
2023-01-06 23:22:12 -0300

    Final commmit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,42 @@
-/* Monte sua lógica aqui */
+async function render() {
+  const ulList = document.querySelector(".pokemon-card-list")
+
+  ulList.innerHTML = ""
+
+  const listaDePokemons = await getAllPokemons()
+
+  listaDePokemons.results.forEach(pokemon => {
+    const numeroNaPokedex = pokemon.url.slice(34, -1)
+
+    ulList.insertAdjacentHTML(
+      "beforeend",
+      `<li class="pokemon-card">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt="${pokemon.name}" class="pokemon-img">
+      <p class="pokemon-name">${pokemon.name}</p>
+    </li>
+`
+    )
+  })
+}
+render()
+
+function renderSearch() {
+  const searchInput = document.querySelector(".search-input")
+  const searchBtn = document.querySelector(".search-button")
+
+  searchBtn.addEventListener("click", () => {
+    getPokemonByName(searchInput.value.toLocaleLowerCase().trim())
+  })
+}
+
+async function renderAll() {
+  const pokemon = await getAllPokemons()
+  const AllPokemons = document.querySelector("#AllPokemons")
+
+  AllPokemons.addEventListener("click", () => {
+    render(pokemon)
+  })
+}
+
+renderSearch()
+await renderAll()
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,46 @@
-/* Monte a lógica das requisições aqui */
+async function getAllPokemons() {
+  const pokemonApi = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=905`,
+    {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json"
+      }
+    }
+  )
+    .then(res => res.json())
+    .then(res => {
+      return res
+    })
+
+  return pokemonApi
+}
+
+async function getPokemonByName(pokemon) {
+  const pokemons = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(res => {
+      const ulPokemonList = document.querySelector(".pokemon-card-list")
+
+      ulPokemonList.innerHTML = ""
+
+      const pokedexNumber = res.species.url.slice(42, -1)
+
+      ulPokemonList.insertAdjacentHTML(
+        "beforeend",
+        `
+            <li class="pokemon-card">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedexNumber}.png" alt="${res.species.name}" class="pokemon-img">
+            <p class="pokemon-name">${res.species.name}</p>
+          </li>
+            `
+      )
+    })
+
+  return pokemons
+}
